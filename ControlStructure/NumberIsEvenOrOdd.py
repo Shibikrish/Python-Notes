0937b2190c646aaa2a67ce2d6bccb0146dc24f1f
Shibi Krishnamoorthy <shibi.cbe@gmail.com>
2025-06-13 10:12:03 +0000

    [Day1]:Control Structure

---
+++ ControlStructure/NumberIsEvenOrOdd.py
@@ -0,0 +1,9 @@
+'''
+https://docs.google.com/document/d/14CyeZ2kMyJbds5b1TFwzrPcsYChR8PxrEtwjhbS51RA/edit?tab=t.0
+'''
+val=int(input("Enter an integervalue:")) # Getting input as integer
+if val%2 == 0: # if the number is divisble by 2 then its a even number
+    print (val, "is an even Number")
+else:
+    print(val, "is an odd Number")
+
---
+++ ControlStructure/SumOfIntegers.py
@@ -0,0 +1,8 @@
+'''
+https://docs.google.com/document/d/14CyeZ2kMyJbds5b1TFwzrPcsYChR8PxrEtwjhbS51RA/edit?tab=t.0
+'''
+
+sum_v=0 # Initailised sum_v as 0
+for i in range (1,51): #Start value 1, End Value: 51 ( to get the value of 1 to 50)
+    sum_v +=i # Add/increment value of each number in  loop
+print("The sum of number from 1 t0 50 is :", sum_v)
