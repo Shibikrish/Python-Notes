6209b702bf9466ef7cb3b44af6a6ca8b4e431975
Shibi Krishnamoorthy <shibi.cbe@gmail.com>
2025-06-13 09:13:13 +0000

    [Day1]:Basic Python programming

---
+++ BasicPython/BasicMathematicalProblem.py
@@ -0,0 +1,8 @@
+a=float(input("Enter First Number:"))
+b=float(input("Enter Second Number:"))
+add=a + b 
+sub=a - b 
+mul=a*b 
+div=a/b
+print("Addition:\t", add , "\nSubtraction:\t", sub, "\nMultiplication:\t", mul , "\nDivision:\t", div)
+
---
+++ BasicPython/PersonaluzedGreetin.py
@@ -0,0 +1,3 @@
+First=input("Enter you First Name:")
+Last=input("Enter your Last Name:")
+print("Hello, " + First + Last + "! Welcome to the Python program")
